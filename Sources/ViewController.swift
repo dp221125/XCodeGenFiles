5280bd61ee9c0cf9755aefc3610e51bec70268d5
Milkyo <dp221125@naver.com>
2020-03-09 18:50:00 +0900

    first commit

---
+++ Sources/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Remove
+//
+//  Created by Seokho on 2020/03/09.
+//  Copyright © 2020 Seokho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        self.view = view
+    }
+}
+
